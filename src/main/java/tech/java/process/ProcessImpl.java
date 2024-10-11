8a9c61674a4f9449519c91ad2c62eb945bd0c42c
Priyobrato.Das <priyodas12@gmail.com>
2024-10-11 17:27:28 +0530

    Init commit

--- src/main/java/tech/java/process/ProcessImpl.java
+++ src/main/java/tech/java/process/ProcessImpl.java
@@ -1,2 +1,28 @@
-package tech.java.process;public class ProcessImpl {
+package tech.java.process;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ProcessImpl {
+  //cmd: tasklist
+  //every process associated with PID
+  //Image Name                     PID Session Name        Session#    Mem Usage
+  //========================= ======== ================ =========== ============
+  //System Idle Process              0 Services                   0          8 K
+  //System                           4 Services                   0      2,720 K
+  //Secure System                  140 Services                   0     73,756 K
+
+  public static void main (String[] args) throws IOException {
+    ProcessBuilder processBuilder = new ProcessBuilder ();
+
+    Map<String, String> environment = processBuilder.environment ();
+    //environment.forEach ((key, value) -> System.out.println (key + value));
+
+    Process process = new ProcessBuilder ("java", "-version").start ();
+
+    System.out.println (process.pid ());
+    var processhandleList = process.children ().toList ();
+
+    processhandleList.forEach (System.out::println);
+  }
 }
--- src/main/java/tech/java/thread/ThreadImpl.java
+++ src/main/java/tech/java/thread/ThreadImpl.java
@@ -1,2 +1,29 @@
-package tech.java.thread;public class ThreadImpl {
+package tech.java.thread;
+
+// thread is essentially a lightweight process , unit of execution with in a given process so
+// single process may contain several thread.
+
+// a thread in a process shares multiple memory and resources and thats why programmer have to
+// deal with concurrent programming.
+
+// time slicing algorithm : processing time for a single processor is shared among multiple
+// processes and threads
+public class ThreadImpl {
+
+  public static void main (String[] args) {
+    System.out.println (Runtime.getRuntime ().availableProcessors ());
+    var threadSet = Thread.getAllStackTraces ().entrySet ();
+    threadSet.forEach (System.out::println);
+
+    /**
+     * Thread[#19,Common-Cleaner,8,InnocuousThreadGroup]=[Ljava.lang.StackTraceElement;@1d81eb93
+     * Thread[#9,Reference Handler,10,system]=[Ljava.lang.StackTraceElement;@7291c18f
+     * Thread[#20,Monitor Ctrl-Break,5,main]=[Ljava.lang.StackTraceElement;@34a245ab
+     * Thread[#1,main,5,main]=[Ljava.lang.StackTraceElement;@7cc355be
+     * Thread[#12,Attach Listener,5,system]=[Ljava.lang.StackTraceElement;@6e8cf4c6
+     * Thread[#10,Finalizer,8,system]=[Ljava.lang.StackTraceElement;@12edcd21
+     * Thread[#21,Notification Thread,9,system]=[Ljava.lang.StackTraceElement;@34c45dca
+     * Thread[#11,Signal Dispatcher,9,system]=[Ljava.lang.StackTraceElement;@52cc8049
+     * */
+  }
 }
